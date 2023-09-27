2f6d201c76ee923ef2df9e4d105b009b1d9ae7a5
Kyle2911 <Kyle.delacruz@goldenabc.com>
2023-09-27 15:37:44 +0800

    add

--- checkout-custom/index.js
+++ checkout-custom/index.js
@@ -4,23 +4,10 @@ import { PickupLogo } from "./pickup-logo";
 import { ContactInformationInit } from "./contact-information";
 import { ShippingAddress } from "./shipping-address";
 import { BillingAddress } from "./billing-address";
-import { CheckoutHelpText } from "./checkout-help-text";
-import { ShippingInformation } from "./shipping-information";
-import { Payment } from "./payment";
-import { Confirmation } from "./confirmation";
-import { GABCNoteForEmployee } from './gabc-note';
-import { ValidateFLNames } from './validate-shipping-names';
-import { Theme } from "./theme-custom";
 Theme();
 window.custard = new Custard([
   PickupLogo,
-  CheckoutHelpText,
   ContactInformationInit,
   ShippingAddress,
-  ShippingInformation,
-  Payment,
-  Confirmation,
-  BillingAddress,
-  GABCNoteForEmployee,
-  ValidateFLNames
+  BillingAddress
 ]);
