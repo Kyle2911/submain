74ee5b0ed6c9607fa55660662459ce44e4ff871a
Kyle2911 <Kyle.delacruz@goldenabc.com>
2023-09-27 15:34:12 +0800

    add

--- checkout-custom/index.js
+++ checkout-custom/index.js
@@ -4,19 +4,23 @@ import { Custard } from "@discolabs/custard-js";
 import { ContactInformationInit } from "./contact-information";
 import { ShippingAddress } from "./shipping-address";
 import { BillingAddress } from "./billing-address";
-// import { CheckoutHelpText } from "./checkout-help-text";
+import { CheckoutHelpText } from "./checkout-help-text";
 import { ShippingInformation } from "./shipping-information";
-// import { Payment } from "./payment";
-// import { Confirmation } from "./confirmation";
-// import { GABCNoteForEmployee } from './gabc-note';
-// import { ValidateFLNames } from './validate-shipping-names';
-// import { Theme } from "./theme-custom";
+import { Payment } from "./payment";
+import { Confirmation } from "./confirmation";
+import { GABCNoteForEmployee } from './gabc-note';
+import { ValidateFLNames } from './validate-shipping-names';
+import { Theme } from "./theme-custom";
 Theme();
 window.custard = new Custard([
   PickupLogo,
   CheckoutHelpText,
+  ContactInformationInit,
+  ShippingAddress,
+  ShippingInformation,
   Payment,
   Confirmation,
+  BillingAddress,
   GABCNoteForEmployee,
   ValidateFLNames
 ]);
