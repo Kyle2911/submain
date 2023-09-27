f9ee70e6518310209e850315b5ab1c1a848e0b06
Kyle2911 <Kyle.delacruz@goldenabc.com>
2023-09-27 15:35:00 +0800

    add

--- checkout-custom/index.js
+++ checkout-custom/index.js
@@ -1,6 +1,6 @@
 import { Custard } from "@discolabs/custard-js";
 
-// import { PickupLogo } from "./pickup-logo";
+import { PickupLogo } from "./pickup-logo";
 import { ContactInformationInit } from "./contact-information";
 import { ShippingAddress } from "./shipping-address";
 import { BillingAddress } from "./billing-address";
